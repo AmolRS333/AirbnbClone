6dbbd91ee2d925b94e921cdf4cbd88da15ce153a
AmolRS333 <amolrs3012@gmail.com>
2025-01-30 15:08:34 +0530

    1st check

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
 });
 
-app.use((err, res) => {
+app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
   res.status(statusCode).render("error", { message });
   // res.status(statusCode).send(message);
