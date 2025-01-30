dd1d25ad20f7814c7198554aade4c328fb745b92
AmolRS333 <amolrs3012@gmail.com>
2025-01-30 14:41:32 +0530

    fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
